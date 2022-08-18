e4cff484eb6425b32dad0996ef21d5d233e38411
Ye Myo Aung <yemyo994@gmail.com>
2022-08-18 18:01:13 +0630

    initial release

---
+++ lib/socket_io.dart
@@ -0,0 +1,12 @@
+library socket_io;
+
+export 'src/server.dart';
+export 'src/namespace.dart';
+export 'src/engine/transport/transports.dart' show Transport, MessageHandler;
+export 'src/engine/transport/jsonp_transport.dart' show JSONPTransport;
+export 'src/engine/transport/polling_transport.dart' show PollingTransport;
+export 'src/engine/transport/websocket_transport.dart' show WebSocketTransport;
+export 'src/socket.dart' show Socket;
+
+export 'package:socket_io_common/src/engine/parser/parser.dart'
+    show PacketParser;
